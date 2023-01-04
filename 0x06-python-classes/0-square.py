44057721e01ff306122165c241c2b03ff96bc314
pascal <georgepascal1010@gmail.com>
2023-01-04 21:51:07 +0000

    first commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+<<<<<<< HEAD
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a square."""
+    pass
+=======
+"""Define  class Square."""
+class Square:
+    """Represents a Square."""
+>>>>>>> 0a0e81f61b1af4ff2c41eefc221ccc71a1a69077
